39664f1ae5480505ee3a0329744a93d6c00d6929
officialcg9998-crypto <officialcg9998@gmail.com>
2026-05-25 00:50:28 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function Home() {
+  return <div style={{color:"white",background:"#040408",minHeight:"100vh",
+    display:"flex",alignItems:"center",justifyContent:"center",fontSize:32}}>
+    Hello World
+  </div>;
+}
+
+export default function App() {
+  return <Home />;
+}
+
 export function Root() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<LandingPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Home />} />
+        <Route path="/dashboard" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
